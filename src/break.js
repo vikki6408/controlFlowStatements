00af340d0b79a36e5c5c394c3d455f1917214a0a
vikki6408 <vikki.dolt@eduvaud.ch>
2024-09-18 22:12:35 +0200

    feat: break on time

--- src/break.js
+++ src/break.js
@@ -1,8 +1,17 @@
 const numbers = [];
+let compteur = 0;
+
 for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
 }
 
 for (let i = 0; i < numbers.length; i++) {
+
+    if (numbers[i] > 50) break;
     console.log(numbers[i]);
+
+    compteur ++
+
 }
+
+console.log(`Nombre d'opération évitée par la machine : ${numbers.length - compteur}`);
