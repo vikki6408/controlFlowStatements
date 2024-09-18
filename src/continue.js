d7894b9f43985ee5ff46deb0a6e3e027b7d1a415
vikki6408 <vikki.dolt@eduvaud.ch>
2024-09-18 22:13:09 +0200

    feat: continue better

--- src/continue.js
+++ src/continue.js
@@ -1,8 +1,22 @@
 const numbers = [];
+let somme = 0;
+let hightestNumber = 0;
+
 for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
 }
 
 for (let i = 0; i < numbers.length; i++) {
+
     console.log(numbers[i]);
+
+    if (numbers[i] < hightestNumber) continue;
+    somme += numbers[i];
+    hightestNumber = numbers[i];
+
 }
+
+
+console.log(`La somme de tout les éléments du tableau est : ${somme}`);
+
+
